fdaa7ccb159c1e46abb9ee11a8a350d7e980c898
Bruno Divino <brunodivino02@gmail.com>
2022-07-27 11:37:48 -0300

    Resultado após vídeo 1.3

--- codigo/bytebank.py
+++ codigo/bytebank.py
@@ -15,8 +15,10 @@ class Funcionario:
         return self._salario
 
     def idade(self):
+        data_nasciemnto_quebrada = self._data_nascimento.split('/')
+        ano_nascimento = data_nasciemnto_quebrada[-1]
         ano_atual = date.today().year
-        return ano_atual - int(self._data_nascimento)
+        return ano_atual - int(ano_nascimento)
 
     def calcular_bonus(self):
         valor = self._salario * 0.1
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from codigo.bytebank import Funcionario
 
-lucas = Funcionario('Lucas Carvalho', '13/03/2000', 1000)
+def teste_idade():
+    funcionario_teste = Funcionario('Teste', '13/03/2000', 1111)
+    print(f'Teste = {funcionario_teste.idade()}')
 
-print(lucas.idade())
+    funcionario_teste1 = Funcionario('Teste', '13/03/1999', 1111)
+    print(f'Teste = {funcionario_teste1.idade()}')
+
+    funcionario_teste2 = Funcionario('Teste', '01/12/1999', 1111)
+    print(f'Teste = {funcionario_teste2.idade()}')
+
+teste_idade()
