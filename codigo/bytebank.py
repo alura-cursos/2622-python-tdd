818d9ad246f302c44f1362fba9cc98bfb38ddbfa
Bruno Divino <brunodivino02@gmail.com>
2022-07-27 11:52:09 -0300

    Resultado após vídeo 3.3

--- codigo/bytebank.py
+++ codigo/bytebank.py
@@ -25,14 +25,16 @@ class Funcionario:
         nome_quebrado = nome_completo.split(' ')
         return nome_quebrado[-1]
 
-    def decrescimo_salario(self):
+
+    def _eh_socio(self):
         sobrenomes = ['Bragança', 'Windsor', 'Bourbon', 'Yamato', 'Al Saud', 'Khan', 'Tudor', 'Ptolomeu']
-        if self._salario >= 100000 and (self.sobrenome() in sobrenomes):
+        return (self._salario >= 100000) and (self.sobrenome() in sobrenomes)
+
+
+    def decrescimo_salario(self):
+        if self._eh_socio():
             decrescimo = self._salario * 0.1
             self._salario = self._salario - decrescimo
-
-
-
 
 
     def calcular_bonus(self):
