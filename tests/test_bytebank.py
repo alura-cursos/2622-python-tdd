78dcf3e9a9e6ac24c51e2ee672c8e66fe1473a50
Bruno Divino <brunodivino02@gmail.com>
2022-07-27 11:44:31 -0300

    Resultado após vídeo 2.2

---
+++ tests/test_bytebank.py
@@ -0,0 +1,11 @@
+from codigo.bytebank import Funcionario
+
+class TestClass:
+    def test_quando_idade_recebe_13_03_2000_deve_retornar_22(self):
+        entrada = '13/03/2000' # Given-Contexto
+        esperado = 22
+
+        funcionario_teste = Funcionario('Teste', entrada, 1111)
+        resultado = funcionario_teste.idade() # When-ação
+
+        assert resultado == esperado  # Then-desfecho
